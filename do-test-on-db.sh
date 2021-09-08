882006471bd8f491ebf9084381c14a286da8e903
Vladimir Golubev <goso@list.ru>
2021-09-08 17:41:54 +0300

    add postgres

--- do-test-on-db.sh
+++ do-test-on-db.sh
@@ -2,13 +2,23 @@
 
 set -x
 
-docker-compose -f mysql.yml up -d
+DB=mysql
+
+if [ -n "$1" ]
+then
+  DB=$1
+fi
+
+echo DB:$DB
+
+docker-compose -f $DB.yml up -d
 sleep 10
 
 docker-compose -f gate.yml up -d
 sleep 10
 
 cd artifacts
+cp application.properties.$DB application.properties
 java -jar aqa-shop.jar &>/dev/null &
 APP_PID=$!
 sleep 20
@@ -17,7 +27,7 @@ cd ..
 ./gradlew test -Dselenide.headless=true
 
 kill $APP_PID
-docker-compose -f mysql.yml down
+docker-compose -f $DB.yml down
 docker-compose -f gate.yml down
 
 google-chrome file://$PWD/build/reports/tests/test/index.html
---
+++ do-test-postgres.sh
@@ -0,0 +1,1 @@
+./do-test-on-db.sh postgres
